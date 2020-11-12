dcb5a71936787e3d45c9d2e95c7e78ddb5880c30
Kevin Kraus <kevin.kraus1@outlook.com>
2020-11-12 01:09:40 +0100

    feature: add PHP API v1.0.0
    Signed-off-by: Kevin Kraus <kevin.kraus1@outlook.com>

---
+++ music-api/Playlist.php
@@ -0,0 +1,93 @@
+<?php
+class Playlist {
+    public $playlist_id;
+    public $name;
+    public $description;
+    public $imageUrl;
+    public $spotifyUrl;
+
+    /**
+     * @return mixed
+     */
+    public function getPlaylistId()
+    {
+        return $this->playlist_id;
+    }
+
+    /**
+     * @param mixed $playlist_id
+     */
+    public function setPlaylistId($playlist_id)
+    {
+        $this->playlist_id = $playlist_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImageUrl()
+    {
+        return $this->imageUrl;
+    }
+
+    /**
+     * @param mixed $imageUrl
+     */
+    public function setImageUrl($imageUrl)
+    {
+        $this->imageUrl = $imageUrl;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSpotifyUrl()
+    {
+        return $this->spotifyUrl;
+    }
+
+    /**
+     * @param mixed $spotifyUrl
+     */
+    public function setSpotifyUrl($spotifyUrl)
+    {
+        $this->spotifyUrl = $spotifyUrl;
+    }
+
+    public function __toString()
+    {
+        return $this->name . ''. $this->description;
+    }
+}
---
+++ music-api/api.php
@@ -0,0 +1,96 @@
+<?php
+
+require_once("Playlist.php");
+
+class API
+{
+    private $clientId = "";
+    private $clientSecret = "";
+    private $playlists = [];
+
+
+    public function getAllPlaylists(): array
+    {
+        $accessToken = self::getAccessToken();
+        $allPlaylists = [];
+        foreach ($this->playlists as $playlist) {
+            $play = self::getPlaylist($playlist, $accessToken);
+            array_push($allPlaylists, $play);
+        }
+        return $allPlaylists;
+    }
+
+    private function getPlaylist($playlistId, $accessToken): Playlist
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => "https://api.spotify.com/v1/playlists/" . $playlistId . "?fields=name%2Cdescription%2Cimages%2Curi%2Cfollowers",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_POSTFIELDS => "",
+            CURLOPT_HTTPHEADER => [
+                "Authorization: Bearer " . $accessToken
+            ],
+        ]);
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+
+        $playlistRaw = json_decode($response);
+
+        // Cast Raw Playlist to class
+        $playlist = new Playlist();
+
+        $playlist->setDescription($playlistRaw->description);
+        $playlist->setImageUrl($playlistRaw->images[0]->url);
+        $playlist->setName($playlistRaw->name);
+        $playlist->setSpotifyUrl($playlistRaw->uri);
+        $playlist->setPlaylistId($playlistId);
+        return $playlist;
+    }
+
+    private function getAccessToken(): string
+    {
+        $requestUrl = "https://accounts.spotify.com/api/token";
+        $requestBody = "";
+        $requestHeaders = "";
+
+        // Request AccessToken from Spotify Web API
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => "https://accounts.spotify.com/api/token",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "grant_type=client_credentials",
+            CURLOPT_HTTPHEADER => [
+                "Authorization: Basic " . base64_encode($this->clientId.":".$this->clientSecret),
+                "Content-Type: application/x-www-form-urlencoded"
+            ],
+        ]);
+
+        $response = curl_exec($curl);
+        $res = json_decode($response);
+        return $res->access_token;
+    }
+
+}
+$api = new API();
+if ($_SERVER["REQUEST_URI"] === "/api/getPlaylists") {
+    echo(json_encode($api->getAllPlaylists()));
+} else {
+    http_response_code(404);
+    die();
+}
