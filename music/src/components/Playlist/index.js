04dd75bb18b8c325b372cfafefcd55745802b4b5
Kevin Kraus <kevin.kraus1@outlook.com>
2020-11-20 01:26:28 +0100

    feature: add tracking events to Playlist Link Click Handler
    Signed-off-by: Kevin Kraus <kevin.kraus1@outlook.com>

--- music/src/components/Playlist/index.js
+++ music/src/components/Playlist/index.js
@@ -2,12 +2,36 @@ import React, {useState} from 'react';
 import styles from './Playlist.module.scss'
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import ReactGA from 'react-ga';
 
 function Playlist(props) {
     const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleClose = () => {
+        setShow(false);
+        ReactGA.event({
+            category: 'User',
+            action: 'Closed Modal without opening Playlist.'
+        });
+    };
+
+    const handleCloseWithClick = (external) => {
+        setShow(false);
+        ReactGA.event({
+            category: 'User',
+            action: 'Opened Playlist',
+            label: external ? 'Browser' : 'App'
+        })
+    };
+
+    const handleShow = () => {
+        setShow(true);
+        ReactGA.event({
+            category: 'User',
+            action: 'Clicked on Playlist',
+            label: props.playlist.name
+        });
+    };
 
 
     return (
@@ -33,10 +57,10 @@ function Playlist(props) {
                 </Modal.Header>
                 <Modal.Body>Please choose whether you have Spotify installed or not.</Modal.Body>
                 <Modal.Footer>
-                    <Button href={props.playlist.externalUrl} variant="secondary" onClick={handleClose}>
+                    <Button href={props.playlist.externalUrl} variant="secondary" onClick={() => handleCloseWithClick(true)}>
                         Open in Browser
                     </Button>
-                    <Button href={props.playlist.spotifyUrl} variant="primary" onClick={handleClose}>
+                    <Button href={props.playlist.spotifyUrl} variant="primary" onClick={() => handleCloseWithClick(false)}>
                         Open in Spotify
                     </Button>
                 </Modal.Footer>
