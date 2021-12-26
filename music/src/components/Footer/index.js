0178f9b498a59090193fa65cc9e469c265467ae6
Kevin Kraus <contact@kevin-kraus.com>
2021-12-26 14:18:37 +0100

    refactor: change url to .net

--- music/src/components/Footer/index.js
+++ music/src/components/Footer/index.js
@@ -8,7 +8,7 @@ function Footer() {
         <div className={styles.container}>
             <p>© 2020 - 2021 by <a href="https://github.com/kevin-kraus">Kevin Kraus</a></p>
             <p>Version: {packageJson.version}</p>
-            <p>Check out the source code <a href="https://github.com/kevin-kraus/music.kevin-kraus.com">here.</a></p>
+            <p>Check out the source code <a href="https://github.com/kevin-kraus/music.kevin-kraus.net">here.</a></p>
             <p><img style={{width: '100px'}} src="https://d0.awsstatic.com/logos/powered-by-aws-white.png"
                     alt="Powered by AWS Cloud Computing"/></p>
         </div>
