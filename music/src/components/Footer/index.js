f17b098ff3bd37a6f1e08fe00931d289822a87fa
Kevin <36297312+kevin-kraus@users.noreply.github.com>
2023-02-06 09:52:04 +0100

    chore: update copyright

--- music/src/components/Footer/index.js
+++ music/src/components/Footer/index.js
@@ -6,7 +6,7 @@ import styles from './Footer.module.scss'
 function Footer() {
     return (
         <div className={styles.container}>
-            <p>© 2020 - 2021 by <a href="https://github.com/kevin-kraus">Kevin Kraus</a></p>
+            <p>© 2020 - 2023 by <a href="https://github.com/kevin-kraus">Kevin Kraus</a></p>
             <p>Version: {packageJson.version}</p>
             <p>Check out the source code <a href="https://github.com/kevin-kraus/music.kevin-kraus.net">here.</a></p>
             <p><img style={{width: '100px'}} src="https://d0.awsstatic.com/logos/powered-by-aws-white.png"
